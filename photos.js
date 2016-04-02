4da080493e25f6928a31d45d67ea25c1a69c2fe5
caproci <caproci@outlook.com>
2016-04-02 00:48:38 -0400

    Update photos.js

--- photos.js
+++ photos.js
@@ -1,8 +1,14 @@
 /*    
-    Program Name:  Photo Gallery Application
+    Program Name:  PhotoCarousel
     Author: Michael Gunter
     Date: 3/23/2016
     Filename: photos.js
+    Copyright 2016
+
+    This file is part of PhotoCarousel.
+    PhotoCarousel is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+    PhotoCarousel is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License along with PhotoCarousel. If not, see http://www.gnu.org/licenses/.
  */
 
 "use strict"; // interpret document contents in JavaScript strict mode
